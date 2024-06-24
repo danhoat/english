2f77b600a2d2cb2464a53a57a15352a7d048dd89
danng <danhoat@gmail.com>
2024-06-24 17:44:27 +0700

    update

--- assets/script.js
+++ assets/script.js
@@ -2,28 +2,16 @@ $(document).ready(function() {
 	
 
 	var audioElement = document.createElement('audio');
-    audioElement.setAttribute('src', 'http://www.uscis.gov/files/nativedocuments/Track%2093.mp3');
-    audioElement.addEventListener("load", function() {
-    audioElement.play();
-    }, true);
+    // audioElement.setAttribute('src', 'http://www.uscis.gov/files/nativedocuments/Track%2093.mp3');
+    // audioElement.addEventListener("load", function() {
+    // audioElement.play();
+    // }, true);
 
     $('.play').click(function() {
     	var mp3 = $(this).attr('media');
     	audioElement.setAttribute('src', mp3);
     	audioElement.play();
     });
-    $('.pause').click(function() {
-    	audioElement.pause();
-    });
 
-
-    // $( ".play" ).each(function() {
-    //   $( this ).addClass( "foo" );
-
-    //   var mp3 = $(this).attr('media');
-
-    //     audioElement.setAttribute('src', mp3);
-    //     audioElement.play();
-    // });
 
 });
