9acd71ed55fd1d656bf4d6d834addd2b674d0976
danng <danhoat@gmail.com>
2024-07-15 19:19:52 +0700

    rename file

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,7 @@ function includeHTML() {
     elmnt = z[i];
     /*search for elements with a certain atrribute:*/
     file = elmnt.getAttribute("w3-include-html");
+    console.log('File html: ', file);
     if (file) {
       /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
